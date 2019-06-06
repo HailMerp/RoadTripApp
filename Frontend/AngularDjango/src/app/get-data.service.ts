10cbabaf44892576cbe9ce679e493aebafb48550
MaciejGolowkin <maciek.golowkin@gmail.com>
2019-06-06 16:13:53 +0200

    Merge upd

--- Frontend/AngularDjango/src/app/get-data.service.ts
+++ Frontend/AngularDjango/src/app/get-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -149,12 +149,12 @@ addMarkerTag(latitude:number,longitude:number,tag:string,description:string){
 
 
 
-get_user_details(firstName:string, lastName:string){
-  this.httpClient.get(this.baseUrl + 'users').subscribe((userDetail)=>{
-    return userDetail;
-});
+// get_user_details(firstName:string, lastName:string){
+//   this.httpClient.get(this.baseUrl + 'users').subscribe((userDetail)=>{
+//     return userDetail;
+// });
 
-}
+// }
 
 
 }
